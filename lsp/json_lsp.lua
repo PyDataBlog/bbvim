4512ea619ae07b26fd73a747f7e07dc443b8a225
Bernard Brenyah <pimpfada@gmail.com>
2025-06-01 22:48:48 +0200

    feat: added manual CRD selector for k8s yaml files

--- lsp/json_lsp.lua
+++ lsp/json_lsp.lua
@@ -6,4 +6,19 @@ return {
 	init_options = {
 		provideFormatter = true,
 	},
+	settings = {
+		json = {
+			format = { enable = true },
+			validate = { enable = true },
+			schemas = (function()
+				local ok, schemastore = pcall(require, "schemastore")
+				if ok then
+					return schemastore.json.schemas()
+				else
+					vim.notify("SchemaStore.nvim not available for JSON LSP", vim.log.levels.WARN)
+					return {}
+				end
+			end)(),
+		},
+	},
 }
--- lua/plugins/schemastore.lua
+++ lua/plugins/schemastore.lua
@@ -1,5 +1,5 @@
 return {
 	"b0o/SchemaStore.nvim",
-	lazy = true,
-	version = false, -- last release is very old, so we just use the latest commits
+	ft = { "json", "jsonc", "yaml", "yml" }, -- Only load for these filetypes
+	version = false, -- last release is way too old
 }
